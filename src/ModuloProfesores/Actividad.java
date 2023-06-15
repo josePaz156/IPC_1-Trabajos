1d21d75bb98addb1522b15feb128dfdf5d571d78
Jose Paz <joseyaquianpaz64@gmail.com>
2023-06-14 19:56:14 -0600

    first commit

---
+++ src/ModuloProfesores/Actividad.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ModuloProfesores;
+
+/**
+ *
+ * @author andre
+ */
+public class Actividad {
+    public String nombreActividad;
+    public String descripcionActividad;
+    public String ponderacionActividad;
+
+    public Actividad(String nombreActividad, String descripcionActividad, String ponderacionActividad) {
+        this.nombreActividad = nombreActividad;
+        this.descripcionActividad = descripcionActividad;
+        this.ponderacionActividad = ponderacionActividad;
+    }
+}
