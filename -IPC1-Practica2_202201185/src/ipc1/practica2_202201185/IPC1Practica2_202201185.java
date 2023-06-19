1ab1c6cf550c2adb1f67e88186700d5a47b8ef9c
Jose Paz <joseyaquianpaz64@gmail.com>
2023-06-19 14:54:21 -0600

    Pedidos

---
+++ -IPC1-Practica2_202201185/src/ipc1/practica2_202201185/IPC1Practica2_202201185.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ipc1.practica2_202201185;
+
+/**
+ *
+ * @author andre
+ */
+public class IPC1Practica2_202201185 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Inicio open = new Inicio();
+        open.setVisible(true);
+    }
+    
+}
---
+++ -IPC1-Practica2_202201185/src/ipc1/practica2_202201185/Producto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ipc1.practica2_202201185;
+
+/**
+ *
+ * @author andre
+ */
+public class Producto {
+    public String producto;
+    public String precio;
+
+    public Producto(String producto, String precio) {
+        this.producto = producto;
+        this.precio = precio;
+    }
+}
